415e2ef822c5b52b6c184aa3b802d4822a0affdd
Hana Táborská <hhaannuullee33@gmail.com>
2021-12-02 19:51:48 +0100

    footer 2.12.

--- menuRoll.js
+++
@@ -1,8 +0,0 @@
-// funkce, roztáhnutí hamburger menu----------
-const hamburger = document.getElementById("hamburger");
-const navUL = document.getElementsById("nav-ul");
-
-hamburger.addEventListener("click", () => {
-  navUL.classList.toggle("show");
-});
-// ------------------------------------
