f93ba2cbcb75ffd8688a8f8c4cd7391af17fd5b5
Hana Táborská <hhaannuullee33@gmail.com>
2021-12-02 22:24:31 +0100

    rolovací menu

---
+++ menu.js
@@ -0,0 +1,11 @@
+const menuHamburger = document.querySelector(".menu__hamburger");
+let menuOpen = false;
+menuHamburger.addEventListener("click", () => {
+  if (menuOpen) {
+    menuHamburger.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuHamburger.classList.remove("open");
+    menuOpen = false;
+  }
+});
