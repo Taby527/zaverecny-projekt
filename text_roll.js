7f35c66f54bf5052601fe281a0af0fdfd2705db2
Hana Táborská <hhaannuullee33@gmail.com>
2021-12-04 18:10:17 +0100

    rozbalovaci text

---
+++ text_roll.js
@@ -0,0 +1,35 @@
+const title = document.getElementById("main__title--1");
+const mainText = document.getElementById("main__text--1");
+
+title.addEventListener("click", () => {
+  mainText.classList.toggle("show");
+});
+
+const title2 = document.getElementById("main__title--2");
+const mainText2 = document.getElementById("main__text--2");
+
+title2.addEventListener("click", () => {
+  mainText2.classList.toggle("show");
+});
+
+
+const title3 = document.getElementById("main__title--3");
+const mainText3 = document.getElementById("main__text--3");
+
+title3.addEventListener("click", () => {
+  mainText3.classList.toggle("show");
+});
+
+const title4 = document.getElementById("main__title--4");
+const mainText4 = document.getElementById("main__text--4");
+
+title4.addEventListener("click", () => {
+  mainText4.classList.toggle("show");
+});
+
+const title5 = document.getElementById("main__title--5");
+const mainText5 = document.getElementById("main__text--5");
+
+title5.addEventListener("click", () => {
+  mainText5.classList.toggle("show");
+});
